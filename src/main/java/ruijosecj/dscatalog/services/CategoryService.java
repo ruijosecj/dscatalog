a1ded56948c347556cc4fed9626c624c97c7880f
ruijosecj <ruijosecj@gmail.com>
2024-06-03 14:53:40 -0300

    Find Category bu id, exception handling

--- src/main/java/ruijosecj/dscatalog/services/CategoryService.java
+++ src/main/java/ruijosecj/dscatalog/services/CategoryService.java
@@ -1,14 +1,17 @@
 package ruijosecj.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import ruijosecj.dscatalog.dto.CategoryDTO;
 import ruijosecj.dscatalog.entities.Category;
 import ruijosecj.dscatalog.repositories.CategoryRepository;
+import ruijosecj.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -16,10 +19,18 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll(){
 		List<Category> list =  repository.findAll();
 		List<CategoryDTO> listDTO = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 		//return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 		return listDTO;
+	}
+	
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id){
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(()-> new EntityNotFoundException("Entity not found"));
+		return new CategoryDTO(entity);
 	}
 }
